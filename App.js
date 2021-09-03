6efd05dd0deeb18218a7c8f84758855111353127
Carururu <cdvsalanga@gmail.com>
2021-09-04 01:05:08 +0800

    Pass props

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Task from './components/Task'
 
 export default function App() {
   return (
@@ -11,6 +11,7 @@ export default function App() {
 
         <View style={styles.items}>
           {/* This is where the tasks will go */}
+          <Task text={'Task 1'} />
         </View>
       </View>
     </View>
---
+++ components/Task.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Task = (props) => {
+  return <Text>{props.text}</Text>
+}
+
+const styles = StyleSheet.create({})
+
+export default Task
